879642fc7af053692270309bd6a31f8a04206ff7
fabwerk90 <fabwerkmeister90@gmail.com>
2025-05-29 11:13:21 +0200

    Add .gitignore and .pre-commit-config.yaml for improved project structure and resource management

---
+++ terraform-config/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+    description = "The GCP project ID where the resources will be created"
+    type        = string
+}
+
+variable "region" {
+    description = "The GCP standard region where resources will be deployed"
+    type        = string
+}
